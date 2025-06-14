f60048f7c2b60e1abfbee1d53c2623553611f4cd
farsuller <farsuller2020@gmail.com>
2025-06-14 19:21:52 +0800

    upgrade sdk target to 35 gradle properties update

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -4,9 +4,9 @@ object ProjectConfig {
 
     const val NAMESPACE = "com.solo.solodaily"
 
-    const val COMPILE_SDK = 34
+    const val COMPILE_SDK = 35
     const val MIN_SDK = 24
-    const val TARGET_SDK = 34
+    const val TARGET_SDK = 35
 
     const val VERSION_CODE = 4
 
